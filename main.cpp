1697116fc81438f0b963a0776e82a78ee4fe67dd
Mai Cong Danh <62579415+wowyman@users.noreply.github.com>
2020-03-31 17:42:54 +0700

    Add files via upload
    thu nghiem SDL

---
+++ main.cpp
@@ -0,0 +1,135 @@
+#include <iostream>
+#include <SDL.h>
+#include <thread>
+#include <chrono>
+using namespace std;
+void logSDLError(std::ostream& os,
+                 const std::string &msg, bool fatal = false);
+
+void logSDLError(std::ostream& os,
+                 const std::string &msg, bool fatal)
+{
+    os << msg << " Error: " << SDL_GetError() << std::endl;
+    if (fatal) {
+        SDL_Quit();
+        exit(1);
+    }
+}
+const int SCREEN_WIDTH = 800;
+const int SCREEN_HEIGHT = 600;
+const string WINDOW_TITLE = "An Implementation of Code.org Painter";
+struct box{
+    int x,y;
+    int size =10;
+    void render(SDL_Renderer* renderer){
+        SDL_Rect filled_rect;
+        filled_rect.x = x;
+        filled_rect.y = y;
+        filled_rect.w = size;
+        filled_rect.h = size;
+        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
+        SDL_RenderFillRect(renderer, &filled_rect);
+
+    }
+    bool inside(int minX,int minY,int maxX,int maxY){
+        return (minX <= x && minY <= y && x+size <= maxX && y+size <= maxY);
+    }
+    int stepX = 5;
+    int stepY = 0;
+
+    void move(){
+        x+= stepX;
+        y+= stepY;
+    }
+    void turnLeft(){
+        stepX = -5;
+        stepY = 0;
+    }
+    void turnRight(){
+        stepX = 5;
+        stepY = 0;
+    }
+    void turnDown(){
+        stepY = 5;
+        stepX = 0;
+    }
+    void turnUp(){
+        stepY = -5;
+        stepX = 0;
+    }
+
+};
+void initSDL(SDL_Window* &window, SDL_Renderer* &renderer);
+void initSDL(SDL_Window* &window, SDL_Renderer* &renderer)
+{
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+        logSDLError(std::cout, "SDL_Init", true);
+
+    window = SDL_CreateWindow(WINDOW_TITLE.c_str(), SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (window == nullptr) logSDLError(std::cout, "CreateWindow", true);
+
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED |
+                                              SDL_RENDERER_PRESENTVSYNC);
+    if (renderer == nullptr) logSDLError(std::cout, "CreateRenderer", true);
+
+    SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "linear");
+    SDL_RenderSetLogicalSize(renderer, SCREEN_WIDTH, SCREEN_HEIGHT);
+}
+void quitSDL(SDL_Window* window, SDL_Renderer* renderer);
+
+void quitSDL(SDL_Window* window, SDL_Renderer* renderer)
+{
+	SDL_DestroyRenderer(renderer);
+	SDL_DestroyWindow(window);
+	SDL_Quit();
+}
+void waitUntilKeyPressed();
+
+void waitUntilKeyPressed()
+{
+    SDL_Event e;
+    while (true) {
+        if ( SDL_WaitEvent(&e) != 0 &&
+             (e.type == SDL_KEYDOWN || e.type == SDL_QUIT) )
+            return;
+        SDL_Delay(100);
+    }
+}
+int main(int argc, char* argv[])
+{
+
+    SDL_Window* window;
+    SDL_Renderer* renderer;
+    initSDL(window, renderer);
+    box box;
+    box.x = 10;
+    box.y = 10;
+    SDL_Event e;
+    while(box.inside(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT)){
+
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        SDL_RenderClear(renderer);
+        box.render(renderer);
+        SDL_RenderPresent(renderer);
+
+        //waitUntilKeyPressed();
+
+        SDL_Delay(10);
+        box.move();
+        if ( SDL_WaitEvent(&e) == 0) continue;
+        if (e.type == SDL_QUIT) break;
+		if (e.type == SDL_KEYDOWN) {
+        	switch (e.key.keysym.sym) {
+        		case SDLK_ESCAPE: break;
+        		case SDLK_a: box.turnLeft(); break;
+            	case SDLK_d: box.turnRight(); break;
+            	case SDLK_s: box.turnDown(); break;
+            	case SDLK_w: box.turnUp(); break;
+        		default: break;
+        }
+        box.move();
+    }
+}
+    quitSDL(window, renderer);
+    return 0;
+}
