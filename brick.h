6b63ec8425d088b3ba21f9c2ca0154820f5fa2ce
Mai Cong Danh 19021233 <62579415+wowyman@users.noreply.github.com>
2020-05-09 10:29:04 +0700

    Add files via upload

--- brick.h
+++ brick.h
@@ -1,24 +1,5 @@
 #include <iostream>
 
-int max(int a,int b,int c,int d)
-{
-    int max=a;
-    if(max<b)
-        max=b;
-    if(max<c)
-        max=c;
-    if(max<d)
-        max=d;
-    return max;
-}
-int min(int a,int b,int c,int d)
-{
-    int min=a;
-    if(min>b) min=b;
-    if(min>c) min=c;
-    if(min>d) min = d;
-    return min;
-}
 struct point
 {
     int x,y;
